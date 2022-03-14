aa66d466faa503b4e0544d8cfd6fb8dce60a5165
luisdaniel-21 <pbohorquezluisdani@gmail.com>
2022-03-14 15:55:15 -0600

    se agrego votocontroller y se agrego el create.blade

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CasillaController;
 use App\Http\Controllers\CandidatoController;
 use App\Http\Controllers\EleccionController;
+use App\Http\Controllers\VotoController;
 
 
 /*
@@ -23,3 +24,4 @@ Route::get('/', function () {
 Route::resource('casilla', CasillaController::class);
 Route::resource('candidato', CandidatoController::class);
 Route::resource('eleccion', EleccionController::class);
+Route::resource('voto', VotoController::class);
